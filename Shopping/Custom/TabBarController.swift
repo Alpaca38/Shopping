d2da94c4db54d29ac0e6851f5ea355e99f1c5cb2
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-06-13 18:38:28 +0900

    패키지 등록, 커스텀 탭바 컨트롤러 생성

---
+++ Shopping/Custom/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  Shopping
+//
+//  Created by 조규연 on 6/13/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
