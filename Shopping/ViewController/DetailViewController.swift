a17c0c5a6b0cd493f4ff0b12e13463d3e02e32a8
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-06-14 20:35:23 +0900

    웹뷰 구현

--- Shopping/ViewController/DetailViewController.swift
+++ Shopping/ViewController/DetailViewController.swift
@@ -6,15 +6,32 @@
 //
 
 import UIKit
+import WebKit
+import SnapKit
 
 class DetailViewController: BaseViewController {
     
     var titleText: String?
     var data: Item?
     
+    lazy var webView = {
+        let view = WKWebView()
+        let url = URL(string: data!.link)!
+        view.load(URLRequest(url: url))
+        self.view.addSubview(view)
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationItem()
+        configureLayout()
+    }
+    
+    func configureLayout() {
+        webView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func setNavigationItem() {
