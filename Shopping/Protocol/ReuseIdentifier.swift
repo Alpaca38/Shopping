993c4c154ccea0cda979bfca8e0c973da8bbaaa0
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-06-27 14:08:30 +0900

    모든 객체에 ReuseIdentifier 적용

--- Shopping/Protocol/ReuseIdentifier.swift
+++ Shopping/Protocol/ReuseIdentifier.swift
@@ -5,20 +5,16 @@
 //  Created by 조규연 on 6/14/24.
 //
 
-import UIKit
+import Foundation
 
 protocol ReuseIdentifier: AnyObject {
     static var identifier: String { get }
 }
 
-extension UICollectionViewCell: ReuseIdentifier {
+extension ReuseIdentifier {
     static var identifier: String {
         return String(describing: self)
     }
 }
 
-extension UITableViewCell: ReuseIdentifier {
-    static var identifier: String {
-        return String(describing: self)
-    }
-}
+extension NSObject: ReuseIdentifier { }
